63213f3581c4fc45ea092276af6dc93d24553f12
Mark Wilden <mark@mwilden.com>
2010-12-05 11:11:17 -0800

    Encode to work file, then move to destination

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -1,10 +1,16 @@
+require 'fileutils'
+
 class Encoder
   attr_reader :duration
 
   def encode input, output
     started_at = Time.now
-    `/Applications/kmttg/ffmpeg/ffmpeg -y -an -i #{quote_for_exec(input)} -threads 2 -croptop 4 -target ntsc-dv #{quote_for_exec(output)}`
+    work = output + '.work'
+    File.delete output if File.exists? output
+    File.delete work if File.exists? work
+    `/Applications/kmttg/ffmpeg/ffmpeg -y -an -i #{quote_for_exec(input)} -threads 2 -croptop 4 -target ntsc-dv #{quote_for_exec(work)}`
     @duration = Time.now - started_at
+    FileUtils.move work, output if File.exists? work
     File.exists? output
   end
 
